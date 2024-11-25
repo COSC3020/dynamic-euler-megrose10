7cd77a993dd792a168def19eb8aa85edd20ad6ea
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-25 14:55:30 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,3 +4,9 @@ eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+
+assert(factorial(1) === 1);
+assert(e(1) === 2);
+
+assert(factorial(8) === 40320);
+assert(e(8) === 2.71827876984);
