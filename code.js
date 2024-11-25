2cc6505f1a68e8cd5b463ac131ef0b41a9e60217
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-25 14:16:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -7,17 +7,20 @@ function e(n) {
     if(n === 0) return 1;
     else return 1.0 / factorial(n) + e(n - 1);
 }*/
+function factorial(n) {
+  let fac = 0;
+  for(let i = 0; i <= n; i++) {
+    if(i==0) {
+      fac = 1;
+    }
+    fac *= i; 
+  }
+  return fac;
+}
+
 function e(n) {
   let denominator = 0;
   for(let i = 0; i <= n; i++) {
-    if(i == 0) {
-      denominator = 1; 
-    }
-    else {
-      for(let j = 1; j <= n; j++) {
-        denominator *= j; 
-      }
-    }
-    eNum += (1/denominator);
+    eNum += (1/(factorial(n)));
   }
 }
