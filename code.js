8e821f9e247e708c614f0e77575b71bb376682c9
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-11-26 11:37:24 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
+function e(n) {
+  let eNum = 1;
+  let fac = 1;
+  for(let i = 1; i <= n; i++) {
+    //let fac = 1;
+    if(fac > 0) {
+      fac *= i;
+      //console.log(fac);
+    }
+    eNum += (1/fac);
+  }
+  return eNum;
+}
+
+//let test = 10;
+//if(factorial(test) === 3628800) {
+//if(e(test) === 2.7182818011463845) {
+//  console.log("true!");
+//}
+//else {
+//  console.log("false...value = " + e(test));
+//}
+
 /*function factorial(n) {
   let fac = 1;
   //console.log("n = " + n);
@@ -14,7 +37,7 @@ if(e(test) === 2.7182818011463845) {
 }
 else {
   console.log("false...value = " + e(test));
-}*/
+}
 
 function e(n) {
   let eNum = 0;
@@ -27,3 +50,4 @@ function e(n) {
   }
   return eNum;
 }
+*/
